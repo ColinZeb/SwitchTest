23e2d250c0384c28a6154dc14db65b313bfbc080
Colin <colin@mmin.us>
2022-02-22 16:50:46 +0800

    添加项目文件。

---
+++ SwitchTest/MyDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchTest
+{
+ 
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=dbtest;Username=postgres;Password=db_dev");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+        public MyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+    }
+
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+
+    }
+}
---
+++ SwitchTest/Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using SwitchTest;
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        var tz = TimeZone.CurrentTimeZone.StandardName;
+        Console.WriteLine(tz);
+        Console.WriteLine("Hello, World!");
+
+        var ob = new DbContextOptionsBuilder();
+        ob.UseNpgsql("Host=localhost;Port=5432;Database=dbtest;Username=postgres;Password=db_dev");
+        using var db = new MyDbContext(ob.Options);
+        //db.Books.FirstOrDefault();
+        //AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+        var today = DateTime.Today;
+        QueryTest(db, today);
+        TestInsert(db, today);
+        TestInsert(db, today.ToUniversalTime());
+        Console.ReadKey();
+    }
+
+    private static void Clear(MyDbContext db)
+    {
+        #region Clear Data
+        var list = db.Books.ToList();
+        db.RemoveRange(list);
+        db.SaveChanges();
+        #endregion
+    }
+
+    static void TestInsert(MyDbContext db, DateTime today)
+    {
+        Console.WriteLine("-------started---------------");
+        Clear(db);
+        Console.WriteLine($"today : {today:O}");
+        var book = new Book() { Name = "Test book 1", CreatedDate = DateTime.Now, ReleaseDate = today };
+        db.Add(book);
+        db.SaveChanges();
+        var local = today.ToLocalTime();
+        var q1 = db.Books.Where(x => x.ReleaseDate.Date == local);
+        var b1 = q1.FirstOrDefault();
+        Console.WriteLine($"b1[local] is null :{b1 == null}");
+
+        var utc = today.ToUniversalTime();
+        var q2 = db.Books.Where(x => x.ReleaseDate == utc);
+        var b2 = q2.FirstOrDefault();
+        Console.WriteLine($"b2[utc] is null :{b2 == null}");
+
+        var q3 = db.Books.Where(x => x.CreatedDate.Date == local);
+        var b3 = q3.FirstOrDefault();
+        Console.WriteLine($"b3[local,trunc] is null :{b3 == null}");
+
+        var q4 = db.Books.Where(x => x.CreatedDate.Date == utc);
+        var b4 = q4.FirstOrDefault();
+        Console.WriteLine($"b4[utc,trunc] is null :{b4 == null}");
+        Console.WriteLine("-------finshed---------------");
+    }
+    static void QueryTest(MyDbContext db, DateTime today)
+    {
+        Console.WriteLine("-------started---------------");
+      
+        Console.WriteLine($"today : {today:O}");
+       
+        //var local = today.ToLocalTime();
+        //var q1 = db.Books.Where(x => x.ReleaseDate.Date == local);
+        //var b1 = q1.FirstOrDefault();
+        //Console.WriteLine($"b1[local] is null :{b1 == null}");
+
+        var utc = today.ToUniversalTime();
+        var q2 = db.Books.Where(x => x.ReleaseDate == utc);
+        var b2 = q2.FirstOrDefault();
+        Console.WriteLine($"b2[utc] is null :{b2 == null}");
+
+        //var q3 = db.Books.Where(x => x.CreatedDate.Date == local);
+        //var b3 = q3.FirstOrDefault();
+        //Console.WriteLine($"b3[local,trunc] is null :{b3 == null}");
+
+        var q4 = db.Books.Where(x => x.CreatedDate.Date == utc);
+        var b4 = q4.FirstOrDefault();
+        Console.WriteLine($"b4[utc,trunc] is null :{b4 == null}");
+        Console.WriteLine("-------finshed---------------");
+    }
+}
